7c69ec2ae61bbf803dffc2f9041c8dfeed032d43
SimSimBappel <simontk2010@hotmail.com>
2023-11-28 12:02:38 +0100

    Serial_bridge converted to a class

--- src/serial2Ros/serial2ros/serial_bridge.py
+++ src/serial2Ros/serial2ros/serial_bridge.py
@@ -7,144 +7,110 @@ import tf2_ros
 import serial
 import math
 
-# Constants
-SERIAL_PORT = "/dev/ttyUSB0"  # Update with your Arduino serial port
-BAUD_RATE = 115200
+class SerialBridge(Node):
+    def __init__(self):
+        super().__init__('serial_bridge')
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
+        self.odometry_publisher = self.create_publisher(Odometry, 'odometry', 10)
+        self.odometry_msg = Odometry()
 
-ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+        self.x, self.y, self.theta = 0.0, 0.0, 0.0
 
-def read_serial():
-    if ser.in_waiting > 0:
-        line = ser.readline().decode('utf-8').rstrip()
-        return line
-    return None
+        self.create_subscription(Twist, '/cmd_vel', self.twist_callback, 10)
 
-def broadcast_transform(x, y, theta, tf_broadcaster):
-    # Create a TransformStamped message
-    transform_msg = TransformStamped()
-    transform_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
-    transform_msg.header.frame_id = "map"
-    transform_msg.child_frame_id = "base_link"
+        self.update_frequency = 100  # Hz
+        self.update_timer = self.create_timer(1.0 / self.update_frequency, self.update_callback)
 
-    # Set translation based on calculated position
-    transform_msg.transform.translation.x = x
-    transform_msg.transform.translation.y = y
-    transform_msg.transform.translation.z = 0.0
+        self.ser = serial.Serial("/dev/ttyUSB0", 115200)
 
-    # Set rotation based on calculated orientation
-    quaternion = tf_transformations.quaternion_from_euler(0.0, 0.0, theta)
-    transform_msg.transform.rotation.x = quaternion[0]
-    transform_msg.transform.rotation.y = quaternion[1]
-    transform_msg.transform.rotation.z = quaternion[2]
-    transform_msg.transform.rotation.w = quaternion[3]
+    def read_serial(self):
+        if self.ser.in_waiting > 0:
+            line = self.ser.readline().decode('utf-8').rstrip()
+            return line
+        return None
 
-    # Broadcast the transform
-    tf_broadcaster.sendTransform(transform_msg)
+    def broadcast_transform(self):
+        transform_msg = TransformStamped()
+        transform_msg.header.stamp = self.get_clock().now().to_msg()
+        transform_msg.header.frame_id = "map"
+        transform_msg.child_frame_id = "base_link"
 
-def publish_odometry(node, odometry_msg, odometry_publisher, x, y, theta):
-    odometry_msg.header.frame_id = 'map'
-    odometry_msg.child_frame_id = 'base'
+        transform_msg.transform.translation.x = self.x
+        transform_msg.transform.translation.y = self.y
+        transform_msg.transform.translation.z = 0.0
 
-    # Set the pose information 
-    odometry_msg.pose.pose.position = Point(x=x, y=y, z=0.0)
-    quaternion = tf_transformations.quaternion_from_euler(0.0, 0.0, theta)
-    # odometry_msg.pose.pose.orientation = Quaternion(x=0.0, y=0.0, z=quaternion[2], w=quaternion[3])
-    odometry_msg.pose.pose.orientation.x = quaternion[0]
-    odometry_msg.pose.pose.orientation.y = quaternion[1]
-    odometry_msg.pose.pose.orientation.z = quaternion[2]
-    odometry_msg.pose.pose.orientation.w = quaternion[3]
+        quaternion = tf_transformations.quaternion_from_euler(0.0, 0.0, self.theta)
+        transform_msg.transform.rotation.x = quaternion[0]
+        transform_msg.transform.rotation.y = quaternion[1]
+        transform_msg.transform.rotation.z = quaternion[2]
+        transform_msg.transform.rotation.w = quaternion[3]
 
-    # Set the twist information
-    odometry_msg.twist.twist.linear = Vector3(x=1.0, y=0.0, z=0.0)
-    odometry_msg.twist.twist.angular = Vector3(x=0.0, y=0.0, z=0.0)
+        self.tf_broadcaster.sendTransform(transform_msg)
 
-    # Set the timestamp
-    # odometry_msg.header.stamp = node.get_clock().now().to_msg()
+    def publish_odometry(self):
+        self.odometry_msg.header.frame_id = 'map'
+        self.odometry_msg.child_frame_id = 'base'
 
-    odometry_publisher.publish(odometry_msg)
+        self.odometry_msg.pose.pose.position = Point(x=self.x, y=self.y, z=0.0)
+        quaternion = tf_transformations.quaternion_from_euler(0.0, 0.0, self.theta)
+        self.odometry_msg.pose.pose.orientation.x = quaternion[0]
+        self.odometry_msg.pose.pose.orientation.y = quaternion[1]
+        self.odometry_msg.pose.pose.orientation.z = quaternion[2]
+        self.odometry_msg.pose.pose.orientation.w = quaternion[3]
 
-def update_pose(x, y, theta, displacement, heading):
-    # Update the robot's pose based on odometry data
-    theta += heading
-    displacement = displacement / 100 # From mm to m
-    x += displacement * math.cos(theta)
-    y += displacement * math.sin(theta)
+        self.odometry_msg.twist.twist.linear = Vector3(x=1.0, y=0.0, z=0.0)
+        self.odometry_msg.twist.twist.angular = Vector3(x=0.0, y=0.0, z=0.0)
 
-    return x, y, theta
+        self.odometry_publisher.publish(self.odometry_msg)
 
-def map_value(value, from_min, from_max, to_min, to_max):
-    return (value - from_min) * ((to_max - to_min) / (from_max - from_min)) + to_min
+    def update_pose(self, displacement, heading):
+        self.theta += heading
+        displacement = displacement / 1000  # From mm to m
+        self.x += displacement * math.cos(self.theta)
+        self.y += displacement * math.sin(self.theta)
 
-def twist_callback(msg):
-    linear_x = msg.linear.x
-    angular_z = msg.angular.z
+    def twist_callback(self, msg):
+        linear_x = msg.linear.x
+        angular_z = msg.angular.z
 
-    if angular_z > math.pi/4:
-        angular_z = 110
-    elif angular_z < -math.pi/4:
-        angular_z = 20
-    else:
-        angular_z = map_value(angular_z, -math.pi/4, math.pi/4, 20, 110) 
-    #max left:110 max right:20
+        if angular_z > math.pi/4:
+            angular_z = 110
+        elif angular_z < -math.pi/4:
+            angular_z = 20
+        else:
+            angular_z = self.map_value(angular_z, -math.pi/4, math.pi/4, 20, 110)
 
+        command = f"{linear_x:.2f},{angular_z:.2f};\n"
 
-    # print(msg.angular.z)
+        self.ser.write(command.encode())
 
-    # Create the command string in the format "linear_x,angular_z"
-    # command = f"{linear_x:.2f},{angular_z:.2f};\n"
+    def map_value(self, value, from_min, from_max, to_min, to_max):
+        return (value - from_min) * ((to_max - to_min) / (from_max - from_min)) + to_min
 
-    command = f"{linear_x:.2f},{angular_z:.2f};\n"
-
-
-    # Send the command to Arduino
-    ser.write(command.encode())
+    def update_callback(self):
+        try:
+            serial_data = self.read_serial()
+            if serial_data is not None :
+                displacement, heading = map(float, serial_data.split(' '))
+                self.update_pose(displacement, heading)
+                self.publish_odometry()
+                self.broadcast_transform()
+        except KeyboardInterrupt:
+            self.ser.close()
 
 def main():
     rclpy.init()
+    node = SerialBridge()
 
-    node = rclpy.create_node("serial_bridge")
-    # Create a TF broadcaster
-    tf_broadcaster = tf2_ros.TransformBroadcaster(node)
-    tf_broadcaster
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
 
-    odometry_publisher = node.create_publisher(Odometry, 'odometry', 10)
-    odometry_msg = Odometry()
-
-    # Create a Twist subscriber
-    twist_subscription = node.create_subscription(
-        Twist,
-        '/cmd_vel',
-        twist_callback,
-        10
-    )
-    twist_subscription
-
-    # Initial pose of the robot
-    x, y, theta = 0.0, 0.0, 0.0
-
-    while rclpy.ok():
-        # Read data from the serial port (adjust parsing logic based on your format)
-        try:
-            serial_data = read_serial()
-            if serial_data is not None:
-                displacement, heading = map(float, serial_data.split(' '))
-
-                # Update the robot's pose
-                x, y, theta = update_pose(x, y, theta, displacement, heading)
-
-                # Broadcast the transform with the updated pose
-                # broadcast_transform(x, y, theta, tf_broadcaster)
-                publish_odometry(node, odometry_msg, odometry_publisher, x, y, theta)
-
-            rclpy.spin_once(node, timeout_sec=0.009)
-
-        except KeyboardInterrupt:
-            ser.close()
-
-    # Close the serial port when exiting
-    ser.close()
-
+    node.ser.close()
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
+
